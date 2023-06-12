1e7efada97c1ffbb73c294f4927d4c6b17021380
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-06-12 13:47:32 -0600

    servlets

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,32 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CountServlet", urlPatterns = "/count")
+
+public class CounterServlet extends HttpServlet {
+
+    private int counter = 0;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+
+        String resetCount = req.getParameter("reset");
+
+
+        if(resetCount != null) {
+            counter = 0;
+            out.println("<h1>Count: " + counter);
+        } else {
+            counter++;
+            out.println("<h1>Count: " + counter);
+        }
+        System.out.println("Count: " + counter);
+
+    }
+}
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,12 +6,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+
+        String firstname = request.getParameter("firstname");
+        System.out.println(firstname);
+
+        if(firstname == null) {
+            out.println("<h1>Hello, World!</h1>");
+        } else {
+            out.println("<h1>Hello, " + firstname + "!</h1>");
+        }
+
     }
 }
